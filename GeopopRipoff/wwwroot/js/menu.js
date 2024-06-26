c1257008f9a6ecc8ea1e30654ff9564e1d281ae9
Sebastian Sokol <sebastiansokol@outlook.it>
2024-06-26 17:25:39 +0200

    Reels

--- GeopopRipoff/wwwroot/js/menu.js
+++ GeopopRipoff/wwwroot/js/menu.js
@@ -4,5 +4,6 @@
     // You can add your logic here to manipulate the DOM or perform actions
     footer.style.display = 'none';
     document.body.style.transition = "background-image 0.5s ease-in-out";
-    document.body.style.backgroundImage = "url('/Img/Background_Home.jpg')";
+    document.body.style.backgroundImage = "url('/Img/BG_Img.jpg')";
+    document.body.style.backgroundSize = "cover";
     });
--- GeopopRipoff/wwwroot/js/site.js
+++ GeopopRipoff/wwwroot/js/site.js
@@ -1,6 +1,46 @@
-﻿function navigateTo(view) {
-    const footer = document.getElementById('footer');
-    if (view === 'Menu') {
-        footer.style.display = 'none';
-    }
-}
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const aDiv = document.getElementById("flash-stories")
+    const videos = document.getElementsByClassName('thumbnail-video');
+    let observer;
+    let currentVideoIndex = 0;
+    let isPlaying = false;
+
+    const playVideoSequence = async () => {
+        while (isPlaying) {
+            const video = videos[currentVideoIndex];
+            video.currentTime = 0;
+            await video.play();
+            await new Promise(resolve => setTimeout(resolve, 2000)); // Play for 2 seconds
+            video.pause();
+            video.currentTime = 0; // Reset the video
+
+            currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+        }
+    };
+
+    const startSequence = () => {
+        isPlaying = true;
+        playVideoSequence();
+    };
+
+    const stopSequence = () => {
+        isPlaying = false;
+        videos.forEach(video => {
+            video.pause();
+            video.currentTime = 0; // Reset all videos
+        });
+        currentVideoIndex = 0;
+    };
+
+    observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                startSequence();
+            } else {
+                stopSequence();
+            }
+        });
+    }, { threshold: 0.5 });
+
+    observer.observe(aDiv);
+});
