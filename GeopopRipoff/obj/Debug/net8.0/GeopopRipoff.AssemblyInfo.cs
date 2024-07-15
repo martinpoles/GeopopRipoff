bf24839ee96e2c3b9734ad07479a11a10ad8a326
Sebastian Sokol <sebastiansokol@outlook.it>
2024-07-15 17:56:31 +0200

    1

--- GeopopRipoff/obj/Debug/net8.0/GeopopRipoff.AssemblyInfo.cs
+++ GeopopRipoff/obj/Debug/net8.0/GeopopRipoff.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeopopRipoff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5111fb1deffa776a7ca4e237ce9fc3d9a250b90b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96c0f25df9415f0575c36d0cc266a28df8b294a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeopopRipoff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeopopRipoff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
